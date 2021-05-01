a1234af446ab0a8c3f0fe6cede99e7c141610b30
+W <willohyayo@gmail.com>
2021-05-01 19:51:36 +0800

    Add project files.

---
+++ Models/Products.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCore5.Api.Lab.Models
+{
+    public class Products
+    {
+
+    }
+}
